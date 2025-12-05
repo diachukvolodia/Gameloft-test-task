5a0e64a32e9524dcb2747c16facc9664d51a09d5
volodya-diachuk <v.diachuk@cheil.com>
2025-12-05 15:56:35 +0200

    update deploy configs

--- src/configs/services/axios.configs.ts
+++ src/configs/services/axios.configs.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { AxiosInstance } from 'axios';
 
 export const axiosClient: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: 'https://fakestoreapi.com',
   headers: {
     'Content-Type': 'application/json',
   },
